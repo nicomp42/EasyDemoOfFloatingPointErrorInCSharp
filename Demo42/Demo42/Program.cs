0bbf2118ef2b86a67f6bcb9b7603ad362f52e03f
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-03-13 21:45:54 -0400

    Added code to main

--- Demo42/Demo42/Program.cs
+++ Demo42/Demo42/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace Demo42 {
     class Program {
         static void Main(string[] args) {
+            float alpha = 42.0F;
+            float delta = .000001F;
+            Console.WriteLine("Floating Point demo...");
+            Console.WriteLine("Start with a float: " + alpha);
+            Console.WriteLine("Add " + delta);
+            alpha += delta;
+            Console.WriteLine("Result = " + alpha);
+            // Whoosh
         }
     }
 }
